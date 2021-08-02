b329bb4b5089a12b5b7143d69798f057edf1fe42
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2021-08-02 17:30:20 -0500

    fixed the 'dragged' effect on the hidden canvas

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 // let scroll = 0
 // temp = 1
-// var cameraZ = 300
+var cameraZ = 300
+var hc;
 
 function setup() {
     createCanvas(400, 400, WEBGL);
@@ -9,9 +10,11 @@ function setup() {
     // meant to be a scalable solution to keeping track of boxes.
     showDict = { "showCube": true }
     x = 0
+    background(220)
 }
 
 function boxes() {
+    push()
     fill(255, 255, 255)
     if (showDict["showCube"] == true) {
         box(50)
@@ -20,8 +23,12 @@ function boxes() {
         fill(255, 0, 255)
         box(50)
     }
+
+    // IMPORTANT-- clear must be called otherwise the hidden canvas will have the "dragged" effect
+    hc.clear()
     hc.fill(0, 255, 0)
     hc.box(50)
+    pop()
 }
 
 function mouseClicked() {
@@ -39,27 +46,9 @@ function areEqualArr(arr1, arr2) {
     return true
 }
 
-function mouseWheel(event) {
-    scroll += event.delta
-    // console.log(scroll)
-    print(event.delta)
-    if (event.delta < 0) {
-        print("scaling down")
-        cameraZ -= 10
-        // hc.scale(0.09, 0.09, 0.09)
-        // scale(0.09,0.09,0.09)
-    }
-    else {
-        print("scaling up")
-        cameraZ += 10
-        //     hc.scale(1.01,1.01,1.01)  
-        //     scale(1.01,1.01,1.01)
-    }
-}
+
 
 function mouseMoved() {
-    // print(hc.get(mouseX, mouseY))
-
     // if the mouse is pointing to a green box in the hidden canvas, we want to not show the other box.
     if (areEqualArr(hc.get(mouseX, mouseY), [0, 255, 0, 255])) {
         showDict["showCube"] = true
@@ -69,26 +58,36 @@ function mouseMoved() {
     }
     print(hc.get(mouseX, mouseY))
     print(showDict)
-
 }
 
-function mcamera(z) {
-    hc.resetMatrix()
-    resetMatrix()
 
+function mouseWheel(event) {
+    print(event.delta)
+    if (event.delta < 0)
+        cameraZ -= 10
+    else
+        cameraZ += 10
+}
+
+
+function mcamera(z) {
+    // hc.resetMatrix()
+    // resetMatrix()
     camera(1, 1, z)
     hc.camera(1, 1, z)
 }
 
-
 function draw() {
+
     background(220);
     hc.background(0)
-
-    // My implementation of orbital control to integrate the hidden canvas
-    // mcamera(cameraZ)
-
     boxes()
+
+    // My implementation of orbital control to integrate the hidden canvas -- Note: cameraZ is modified by 'mouseWheel' before getting passed.
+    mcamera(cameraZ)
+
+    // hc.background(0)
+
     loadPixels()
     hc.loadPixels()
 }
